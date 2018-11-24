2eb3356457128407643293c71c7c1ef8b64b8257
Adam Dudley <dudleyadam@gmail.com>
2018-11-24 05:02:34 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(card_total)
+def runner(new_card_total)
   welcome
   initial_round
   new_card_total = hit?
